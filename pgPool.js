a60ed8c88d4589d353405673c8fbe54401f667fc
jackp <jackpetersenmusic@gmail.com>
2023-12-18 12:05:13 -0800

    fixed

--- pgPool.js
+++ pgPool.js
@@ -3,11 +3,11 @@ const url = require('url');
 require('dotenv').config();
 
 const config = {
-  user: 'ubuntu',
-  host: 'localhost',
-  database: 'nbastatistics',
-  password: 'redsox45',
-  port: 5432
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT
 };
 
 const pool = new Pool(config);
